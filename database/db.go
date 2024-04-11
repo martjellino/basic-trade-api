dd1fbfc0be787a79aa8e9db8b9e21992a3e63e97
martjellino <mryanmarcellino@gmail.com>
2024-04-11 09:41:59 +0700

    fix: comment .load() in godotenv

--- database/db.go
+++ database/db.go
@@ -5,52 +5,52 @@ import (
 	"fmt"
 	"log"
 	"os"
-	"github.com/joho/godotenv"
-  _ "github.com/lib/pq"
+	// "github.com/joho/godotenv"
+	_ "github.com/lib/pq"
 )
 
 func StartDB() *sql.DB {
-	err := godotenv.Load()
-	if err != nil {
-		log.Fatal("Error loading .env file")
-	}
+	// err := godotenv.Load()
+	// if err != nil {
+	// 	log.Fatal("Error loading .env file")
+	// }
 
 	user := os.Getenv("PG_USER")
 	password := os.Getenv("PG_PASSWORD")
 	host := os.Getenv("PG_HOST")
 	dbname := os.Getenv("PG_DBNAME")
-	 sslmode := os.Getenv("PG_SSLMODE")
-  port := os.Getenv("PG_PORT")
+	sslmode := os.Getenv("PG_SSLMODE")
+	port := os.Getenv("PG_PORT")
 
-	 config := fmt.Sprintf("host=%s port=%v user=%s password=%s dbname=%s sslmode=%s", host, port, user, password, dbname,sslmode)
+	config := fmt.Sprintf("host=%s port=%v user=%s password=%s dbname=%s sslmode=%s", host, port, user, password, dbname, sslmode)
 
-	 db, err := sql.Open("postgres", config)
-	 if err != nil {
-	 	panic(err)
-	 }
+	db, err := sql.Open("postgres", config)
+	if err != nil {
+		panic(err)
+	}
 
 	// Constructing connection string
-//	connStr := fmt.Sprintf("postgresql://%s:%s@%s/%s?sslmode=%s", user, password, host, dbname, sslmode)
+	//	connStr := fmt.Sprintf("postgresql://%s:%s@%s/%s?sslmode=%s", user, password, host, dbname, sslmode)
 
 	// Opening a connection to the database
 	//db, err := sql.Open("postgres", connStr)
 	//if err != nil {
-	 // log.Fatal(err)
+	// log.Fatal(err)
 	//}
 	//defer db.Close()
-  
+
 	rows, err := db.Query("select version()")
 	if err != nil {
-	  log.Fatal(err)
+		log.Fatal(err)
 	}
 	defer rows.Close()
-  
+
 	var version string
 	for rows.Next() {
-	  err := rows.Scan(&version)
-	  if err != nil {
-		log.Fatal(err)
-	  }
+		err := rows.Scan(&version)
+		if err != nil {
+			log.Fatal(err)
+		}
 	}
 	fmt.Printf("version=%s\n", version)
 
